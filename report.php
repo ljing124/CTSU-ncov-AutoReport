5e8b6ad85e9cd48168cc277703179f76a180b7a5
ljing124 <lj19971229>
2020-08-31 18:15:47 +0800

    bug fix

--- report.php
+++ report.php
@@ -1,65 +1,109 @@
 <?php
+/**
+ * CTSU健康打卡平台自动打卡
+ * 
+ * @author LJING
+ * @version 2.0
+ */
     require_once __DIR__ . '/vendor' . '/autoload.php';
     use Goutte\Client;
     
     ignore_user_abort();
 
-    function report(string $username, string $password, array $reportdata){
-        $client = new Client();
-        $crawler = $client->request('GET', 'https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin',);
+    class AutoReport
+    {
+        private $username;
+        private $password;
+        private $reportdata;
+        private $client;
+        private $crawler;
         
-        $form = $crawler->filter('#login')->form();
-        $form['username'] = $username;
-        $form['password'] = $password;
-        $crawler = $client->submit($form);    
-    
-        if($crawler->filter('#wrapper > div.header > ul.nav.navbar-nav.pull-right > li > span')->count()==0){
-            return array('status'=>-1, 'name'=>$username, 'time'=>date('Y-m-d H:i:s'), 'note'=>'身份认证失败');
+        function __construct(string $username, string $password, array $reportdata){
+            $this->username = $username;
+            $this->password = $password;
+            $this->reportdata = $reportdata;
+            $this->client = new Client();
+        }
+        
+        /**
+         * 统一身份认证登录
+         * @access public
+         */
+        public function login(){
+            $this->crawler = $this->client->request('GET', 'https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin',);
+            $form = $this->crawler->filter('#login')->form();
+            $form['username'] = $this->username;
+            $form['password'] = $this->password;
+            $this->crawler = $this->client->submit($form);
+            if($this->crawler->filter('#wrapper > div.header > ul.nav.navbar-nav.pull-right > li > span')->count()>0)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
         }
 
-        $form = $crawler->filter('#report-submit-btn')->form();
-        $form->disableValidation();
-        $form['now_address'] = $reportdata['now_address'];
-        $form['gps_now_address'] = $reportdata['gps_now_address'];
-        $form['now_province'] = $reportdata['now_province'];
-        $form['gps_province'] = $reportdata['gps_province'];
-        $form['now_city'] = $reportdata['now_city'];
-        $form['gps_city'] = $reportdata['gps_city'];
-        $form['now_detail'] = $reportdata['now_detail'];
-        $form['is_inschool'] = $reportdata['is_inschool'];
-        $form['body_condition'] = $reportdata['body_condition'];
-        $form['body_condition_detail'] = $reportdata['body_condition_detail'];
-        $form['now_status'] = $reportdata['now_status'];
-        $form['now_status_detail'] = $reportdata['now_status_detail'];
-        $form['has_fever'] = $reportdata['has_fever'];
-        $form['last_touch_sars'] = $reportdata['last_touch_sars'];
-        $form['last_touch_sars_date'] = $reportdata['last_touch_sars_date'];
-        $form['last_touch_sars_detail'] = $reportdata['last_touch_sars_detail'];
-        $form['other_detail'] = $reportdata['other_detail'];
-        $crawler = $client->submit($form);
-    
-        $realname = $crawler->filter('#wrapper > div.header > ul.nav.navbar-nav.pull-right > li > span')->text();
-        $datestr = $crawler->filter('#daliy-report > form > div > div.form-group.clearfix > span > strong')->text();
-        $datestr = str_replace('*上次上报时间：','',$datestr);
-        $datestr = str_replace('，请每日按时打卡','',$datestr);
-        $reportdate = strtotime($datestr);
-        if($crawler->filter('#wrapper > div.page-wrap > div.flash-message.mgb10.pd020 > p')->count()>0){
-            $errnote = $crawler->filter('#wrapper > div.page-wrap > div.flash-message.mgb10.pd020 > p')->text();
-        } else {
-            $errnote = '未知错误';
-        }
+        /**
+         * 健康上报
+         * @access public
+         */
+        public function report(){
+            $form = $this->crawler->filter('#report-submit-btn')->form();
+            $form->disableValidation();
+            $form['now_address']            = $this->reportdata['now_address'];
+            $form['gps_now_address']        = $this->reportdata['gps_now_address'];
+            $form['now_province']           = $this->reportdata['now_province'];
+            $form['gps_province']           = $this->reportdata['gps_province'];
+            $form['now_city']               = $this->reportdata['now_city'];
+            $form['gps_city']               = $this->reportdata['gps_city'];
+            $form['now_detail']             = $this->reportdata['now_detail'];
+            $form['is_inschool']            = $this->reportdata['is_inschool'];
+            $form['body_condition']         = $this->reportdata['body_condition'];
+            $form['body_condition_detail']  = $this->reportdata['body_condition_detail'];
+            $form['now_status']             = $this->reportdata['now_status'];
+            $form['now_status_detail']      = $this->reportdata['now_status_detail'];
+            $form['has_fever']              = $this->reportdata['has_fever'];
+            $form['last_touch_sars']        = $this->reportdata['last_touch_sars'];
+            $form['last_touch_sars_date']   = $this->reportdata['last_touch_sars_date'];
+            $form['last_touch_sars_detail'] = $this->reportdata['last_touch_sars_detail'];
+            $form['other_detail']           = $this->reportdata['other_detail'];
+            $this->crawler = $this->client->submit($form);
+            $realname = $this->crawler->filter('#wrapper > div.header > ul.nav.navbar-nav.pull-right > li > span')->text();
+            $datestr = $this->crawler->filter('#daliy-report > form > div > div.form-group.clearfix > span > strong')->text();
+            $datestr = str_replace('*上次上报时间：','',$datestr);
+            $datestr = str_replace('，请每日按时打卡','',$datestr);
+            $reportdate = strtotime($datestr);
+            
+            if($this->crawler->filter('#wrapper > div.page-wrap > div.flash-message.mgb10.pd020 > p')->count()>0)
+            {
+                $errnote = $this->crawler->filter('#wrapper > div.page-wrap > div.flash-message.mgb10.pd020 > p')->text();
+            } 
+            else 
+            {
+                $errnote = '未知错误';
+            }
 
-        if(time()-$reportdate<60){
-            return array('status'=>1, 'name'=>$realname, 'time'=>$datestr, 'note'=>'--');
-        } else {
-            return array('status'=>-1, 'name'=>$realname, 'time'=>date('Y-m-d H:i:s'), 'note'=>$errnote);
+            if(time()-$reportdate<60)
+            {
+                return array('status'=>true, 'name'=>$realname, 'time'=>$datestr, 'note'=>'--');
+            } 
+            else 
+            {
+                return array('status'=>false, 'name'=>$realname, 'time'=>date('Y-m-d H:i:s'), 'note'=>$errnote);
+            }
         }
     }
-    
-    $username = $_GET['username'];
-    $password = $_GET['password'];
 
-    $result = report($username, $password, array(
+    $username = ( isset($_GET['username']) ? $_GET['username'] : (isset($argv[1])?$argv[1]:'');
+    $password = ( isset($_GET['password']) ? $_GET['password'] : (isset($argv[2])?$argv[2]:''))
+
+    $username = $argv[1];
+    $password = $argv[2];
+
+    /* 在这里修改健康上报数据 */
+    $reporter = new AutoReport($username, $password, array(
         'now_address'            => '1',
         'gps_now_address'        => '',
         'now_province'           => '340000',
@@ -78,7 +122,22 @@
         'last_touch_sars_detail' => '',
         'other_detail'           => ''
     ));
-    
-    echo $result['time'].' | '.$result['name'].' | 状态：'.$result['status'].' | '.$result['note'];
+
+    if($reporter->login())
+    {
+        $result = $reporter->report();
+        if($result['status'])
+        {
+            echo '1';
+        }
+        else
+        {
+            echo '0';
+        }
+    }
+    else
+    {
+        echo '-1';
+    }
 
 ?>

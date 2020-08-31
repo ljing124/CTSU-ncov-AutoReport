d1462225a3faed996fb0c854ae3a47317238b5e8
ljing124 <lj19971229>
2020-08-31 23:04:03 +0800

    change useragent

--- vendor/symfony/browser-kit/AbstractBrowser.php
+++ vendor/symfony/browser-kit/AbstractBrowser.php
@@ -126,7 +126,7 @@ abstract class AbstractBrowser
     public function setServerParameters(array $server)
     {
         $this->server = array_merge([
-            'HTTP_USER_AGENT' => 'Symfony BrowserKit',
+            'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
         ], $server);
     }
 
